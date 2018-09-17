7e7930669d554ebd44e31de5d06e02e938bf249e
cxm <cui1@vip.qq.com>
2018-09-17 12:02:11 +0800

    重命名repository

--- src/Generators/RepositoryGenerator.php
+++ src/Generators/RepositoryGenerator.php
@@ -21,7 +21,7 @@ class RepositoryGenerator extends BaseGenerator
     {
         $this->commandData = $commandData;
         $this->path = $commandData->config->pathRepository;
-        $this->fileName = $this->commandData->modelName.'Repository.php';
+        $this->fileName = $this->commandData->modelName.'Service.php';
     }
 
     public function generate()
